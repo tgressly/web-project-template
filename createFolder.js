4dc641d48b2e89b9496fb670fe22b81146026d87
Thomas Gressly <founder@foundit.ch>
2016-03-21 14:47:27 +0100

    Add optional title as 2nd argument

--- createFolder.js
+++ createFolder.js
@@ -10,8 +10,9 @@ if (!program.args.length) {
   program.help();
 } else {
   var file = program.args[0];
+  var title = program.args[1] || file;
   var fs = require('fs');
-  var htmlString = '<!doctype html>\n<html lang="de">\n\n<head>\n  <meta charset="utf-8">\n  <title>' + file + '</title>\n  <link rel="stylesheet" href="css/style.css" />\n</head>\n\n<body>\n\n  <script src="js/app.js"></script>\n</body>\n\n</html>\n';
+  var htmlString = '<!doctype html>\n<html lang="de">\n\n<head>\n  <meta charset="utf-8">\n  <title>' + title + '</title>\n  <link rel="stylesheet" href="css/style.css" />\n</head>\n\n<body>\n\n  <script src="js/app.js"></script>\n</body>\n\n</html>\n';
   fs.mkdirSync(file);
   fs.writeFileSync(file + '/index.html', htmlString);
   fs.mkdirSync(file + '/css');
